dc75fbda7a46d722bdd1f943308775df94eedf38
I_L_Y_A_S　ツ <144107542+ILYASkhan420@users.noreply.github.com>
2023-10-10 10:21:25 +0500

    Update ILYAS-1K.py
    rm -rf ILYAS-1K
    git clone https://github.com/ILYASkhan420/ILYAS-1K.git
    ls
    cd ILYAS-1K
    python ILYAS-1K py

--- ILYAS-1K.py
+++ ILYAS-1K.py
@@ -1,6 +1,6 @@
 from os import path
 import os,base64,zlib,pip,urllib
-os.system('xdg-open https://facebook.com/groups/1210513956551911/')
+os.system('xdg-open https://youtube.com/@ILYAS_II?si=rIP7M3o7JuX92C8b/')
 print('\n\033[1;37mCHEAKING YOUR PHONE LICENSE...\n \033[1;32mYOUR LICENSE APPROVED...')
 from os import path
 import os,base64,zlib,pip,urllib
@@ -114,7 +114,7 @@ def CRACKafghan():
 			limit = 50000
 		clear()
 		print('\033[1;33m [1] \033[1;33mSUBSCRIB TO CRACK')
-		os.system('xdg-open   https://youtube.com/@ILYAS_II?si=rIP7M3o7JuX92C8b')
+		os.system('xdg-open   https://youtube.com/@ILYAS_II?si=rIP7M3o7JuX92C8b)
 		linex()
 		mthd = input('\033[1;32m[+]CHOOSE: ')
 		clear()
